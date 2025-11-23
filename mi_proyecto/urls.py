657193219d08f5b9f4e03eae19ba8aa2ec4de1e9
TuNombre <tu@email.com>
2025-11-23 15:38:31 -0300

    Creo nueva vista about, template y ruta

--- mi_proyecto/urls.py
+++ mi_proyecto/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from mi_app import views
+from mi_app.views import about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('about/', about, name='about'),
 ]
